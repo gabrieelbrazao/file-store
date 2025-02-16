0587692a2ab89a05934600b4cedea2be84e8605f
gabrieelbrazao <gabrieelbrazao@gmail.com>
2025-02-16 18:21:33 -0300

    remove type errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,7 @@ export default tseslint.config(
       sourceType: 'module',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: import.meta.url,
       },
     },
   },
@@ -29,7 +29,13 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 );
